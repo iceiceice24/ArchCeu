3976f71505a8a49c69dbc0a6ca4795ac4ff96298
Adoshhu <adrianumalir@gmail.com>
2023-04-14 11:49:31 +0800

    google auth finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-
-  devise_for :users 
+  
+  devise_for :users, controllers:{
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :folders
   root 'folders#home'
   get '/alluser', to: 'folders#alluser', as: :alluser
+  get '/search_folders', to: 'folders#search', as: 'search_folders'  
+  delete "attachments/:id/purge", to: "attachments#purge", as: "purge_attachment"
 
   resources :users do
     resources :folders, only: [:index, :new, :create]
@@ -15,12 +21,7 @@ Rails.application.routes.draw do
     delete 'delete_user', on: :member
   end
   end
-  
-  
 
-  get '/search_folders', to: 'folders#search', as: 'search_folders'
-  get 'folders/back'
+\
 
-  
-  delete "attachments/:id/purge", to: "attachments#purge", as: "purge_attachment"
 end
